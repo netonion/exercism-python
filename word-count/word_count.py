c7c98dfccdf22861dc733df44bf9f2e324add6d8
Sam Guo <netonion@hotmail.com>
2017-08-19 18:49:11 -0400

    Word count with regex and Counter

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,17 +1,6 @@
+import re
+from collections import Counter
+
 def word_count(text):
-
-  tally = {}
-  word  = ''
-
-  for c in text.lower():
-    if c.isalnum():
-      word += c
-    elif word:
-      tally[word] = tally.get(word, 0) + 1
-      word = ''
-    else:
-      pass
-
-  if word: tally[word] = tally.get(word, 0) + 1
-
-  return tally
+  words = re.findall('[^\W_]+', text.lower())
+  return dict(Counter(words))
