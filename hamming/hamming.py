d36923f50be399a5c4a025979c537b0e3d30abfb
Sam Guo <netonion@hotmail.com>
2017-08-19 17:38:56 -0400

    Hamming with comprehension

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,10 +1,3 @@
 def distance(s1, s2):
-  if len(s1) != len(s2):
-    raise ValueError
-
-  count = 0
-  for i, x in enumerate(s1):
-    if x != s2[i]:
-      count += 1
-
-  return count
+  if len(s1) != len(s2): raise ValueError
+  return sum(x != y for x, y in zip(s1, s2))
